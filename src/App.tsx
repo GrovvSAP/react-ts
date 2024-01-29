97812d7d9e8be2a50fcb6c78d344b3a5f1da2228
Yaniv Idov <yaniv.idov@grovv.co.il>
2024-01-29 21:47:15 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Button from "./Components/Button";
+import Alert from "./Components/Alert";
+import { useState } from "react";
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
